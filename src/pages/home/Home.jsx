7ad55ebdf324ff32fe3479bcbd3b98cd47e3fbbc
Mohd Haroon <haroonsaifi2001@gmail.com>
2023-09-11 22:19:14 +0530

    some small changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,15 +3,21 @@ import NavBar from '../../components/navbar/NavBar.jsx'
 import Solutions from '../../components/solutions/Solutions.jsx'
 import UseCase from '../../components/UseCase/UseCase.jsx'
 import Counter from '../../components/counter/Counter.jsx'
+import Hero from '../../components/hero/Hero.jsx'
+import Blog from '../../components/blog/Blog.jsx'
+import Footer from '../../components/footer/footer.jsx'
 
 
 function Home() {
   return (
     <>
       <NavBar />
+      <Hero/>
       <Solutions/>
       <UseCase/>
       <Counter/>
+      <Blog/>
+      <Footer/>
     </>
   )
 }
